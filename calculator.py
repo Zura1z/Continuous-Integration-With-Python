d669469cbd1f3175ecccfdf35c5c5ad5cbfb0b15
Zuraiz Zahoor Ajaz <59668507+Zura1z@users.noreply.github.com>
2022-02-19 19:53:53 +0500

    simple functions

---
+++ calculator.py
@@ -0,0 +1,9 @@
+"""
+Calculator library containing basic math operations.
+"""
+
+def add(first_term, second_term):
+    return first_term + second_term
+
+def subtract(first_term, second_term):
+    return first_term - second_term
