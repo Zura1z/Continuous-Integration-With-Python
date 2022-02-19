a32163da427921ea62b76fed87d8b2892fa4eb67
Zuraiz Zahoor Ajaz <59668507+Zura1z@users.noreply.github.com>
2022-02-19 20:00:56 +0500

    testing bad function

--- test_calculator.py
+++ test_calculator.py
@@ -12,3 +12,5 @@ class TestCalculator:
 
     def test_subtraction(self):
         assert 2 == calculator.subtract(4, 2)
+    def test_multiplication(self):
+        assert 100 == calculator.multiply(10, 10)
